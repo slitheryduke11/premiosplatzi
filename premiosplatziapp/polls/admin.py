e371fda1cf9c84627503ed061f92d20c7e0cd20b
Hedguhar Domínguez <hedguhar@vauxoo.com>
2023-08-30 01:23:20 -0500

    add admin for models

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
+
+
+admin.site.register(Question)
+
+# class ChoicesInline(admin.StackedInline):
+#     model = Choice
+#     can_delete = False
+#     verbose_name_plural = 'choices'
+
+# @admin.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     inlines = (ChoicesInline,)
+
+
+
+
+# class ChoiceAdmin(admin.ModelAdmin):
+#     readonly_fields = ['votes']  # Campos de solo lectura
+
+# admin.site.register(Choice, ChoiceAdmin)
